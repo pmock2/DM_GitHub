e2abaf4e2ded862fa01b49c8f00a844fde0390b4
Daylonz <daylonjanis@gmail.com>
2016-09-30 19:20:43 -0400

    Create Team.java

---
+++ Debate Management/src/teamHarambe/Team.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Daylon on 9/28/2016.
+ */
+public class Team {
+    String name;
+    double score;
+
+    public Team(String name, double score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
