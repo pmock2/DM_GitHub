29db7d98019d6fd6361311204b5895ebaca5d7ba
Daylon <daylonjanis@gmail.com>
2016-10-21 21:52:04 -0400

    Updated client to show error if server is not running.

--- DMClient/src/teamHarambe/Client.java
+++ DMClient/src/teamHarambe/Client.java
@@ -1,6 +1,7 @@
 package teamHarambe;
 import javafx.application.Application;
 
+import javax.swing.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -10,27 +11,38 @@ import java.net.UnknownHostException;
 
 public class Client {
 
+	static Socket s;
+	static BufferedReader fromServer;
+	static PrintStream toServer;
+	static boolean serverRunning = false;
+
 	public static void main(String[] args) throws UnknownHostException, IOException {
-		Socket s = new Socket("127.0.0.1", 1234);
-		BufferedReader fromServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
-		PrintStream toServer = new PrintStream(s.getOutputStream());
-
-		//START INITIAL CHECK FOR SERVER SETUP
-		if (MethodProvider.checkForSetup() == false)
-		{
-			Application.launch(GUI.SuperUserSetup_1.class, args);
+		try {
+			s = new Socket("127.0.0.1", 1234);
+			fromServer = new BufferedReader(new InputStreamReader(s.getInputStream()));
+			toServer = new PrintStream(s.getOutputStream());
+			serverRunning = true;
 		}
-		else
+		catch(Exception e)
 		{
-			Application.launch(GUI.Main_3.class, args);
+			JOptionPane.showMessageDialog(null, "Please make sure the server is running.", "Unable to pinpoint server", JOptionPane.ERROR_MESSAGE);
 		}
 
+		if (serverRunning) {
+
+			//START INITIAL CHECK FOR SERVER SETUP
+			if (MethodProvider.checkForSetup() == false) {
+				Application.launch(GUI.SuperUserSetup_1.class, args);
+			} else {
+				Application.launch(GUI.Main_3.class, args);
+			}
 
 
-		toServer.println("Get_Schedule");
-		System.out.println("Sent request for schedule to server.");
-		while (true) {
-			System.out.println(fromServer.readLine());
+			toServer.println("Get_Schedule");
+			System.out.println("Sent request for schedule to server.");
+			while (true) {
+				System.out.println(fromServer.readLine());
+			}
 		}
 	}
 }
