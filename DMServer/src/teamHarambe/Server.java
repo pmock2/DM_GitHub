67907f2fa25ed8bc05c0d8d32ebb68f762c1b6ae
Drew <dwarwick96@gmail.com>
2016-10-22 23:24:10 -0400

    Fleshed out schedule creation options and improved schedule receive on client

--- DMServer/src/teamHarambe/Server.java
+++ DMServer/src/teamHarambe/Server.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
+import java.util.Calendar;
 
 public class Server {
 	static List<Team> teams = new LinkedList<>();
@@ -19,43 +20,28 @@ public class Server {
 	
 	public static void main(String[] args) throws IOException {
 		ServerSocket server = new ServerSocket(1234);
-		int numTeams = 0;
 
 		(new Thread(){
 			public void run(){
 				Application.launch(GUI.ServerMenu.class, args);
 			}
 		}).start();
+		
 		System.out.println("Welcome to first time setup!");
 		System.out.print("Please create a password: ");
 		System.out.println("Your password is " + console.nextLine());
-		System.out.println("How many teams will play in the debate season?");
-		boolean valid = false;
-		while(numTeams < 2)
-		{
-			try
-			{
-				numTeams =  (int) Double.parseDouble(console.nextLine());
-
-			}
-			//checks input to make sure it is a number
-			catch (NumberFormatException ex)
-			{
-				System.out.println("Invalid input, try again:");
-			}
-		}
 		
-//		try 
-//		{
-//			numTeams = console.nextInt();
-//		}
-//		catch(InputMismatchException e)
-//		{
-//			console.next();
-//		}
+		System.out.println("How many teams will play in the debate season?");
+		int numTeams = promptInt(2);
 		promptTeams(numTeams);
 		
-		List<Referee> referees = generateRefereeList();
+		System.out.println("How many referees will manage the season?");
+		int numReferees = promptInt(numTeams/2);		
+		List<Referee> referees = generateRefereeList(numReferees);
+		
+		System.out.println("Enter the season start date.");
+		Calendar startDate = promptDate();
+	
 		Schedule schedule = new Schedule(teams, referees);
 		
 		System.out.println("Server ready to accept clients.");
@@ -67,20 +53,55 @@ public class Server {
 		}
 	}
 	
+	static int promptInt(int minValue) {
+		int input = 0;
+		
+		do {
+			try {
+				input =  console.nextInt();
+				if (input < minValue) {
+					System.out.print("Please input an integer >= " + minValue + ": ");
+				}
+			}
+			catch (NumberFormatException e) {
+				System.out.print("Please input an integer >= " + minValue + ": ");
+			}
+		} while (input < minValue);
+		
+		return input;
+	}
+	
 	static void promptTeams(int numTeams) {
         System.out.println("Please enter " + numTeams + " team names.");
         for (int i = 0; i < numTeams; i++) {
             System.out.print((i + 1) + ". ");
-            
             teams.add(new Team(console.next()));
             
         }
     }
 	
-	private static List<Referee> generateRefereeList() {
+	static Calendar promptDate() {
+		Calendar selectedDate = Calendar.getInstance();//shitty Java library
+		int selectedYear, selectedMonth, selectedDay;
+		int currentYear = selectedDate.get(Calendar.YEAR);
+		int currentMonth = selectedDate.get(Calendar.MONTH);
+		int currentDay = selectedDate.get(Calendar.DAY_OF_MONTH);
+		
+		System.out.print("Enter the season start year: ");
+		selectedYear = promptInt(currentYear);
+		System.out.print("Enter the season start month: ");
+		selectedMonth = promptInt(selectedYear == currentYear ? currentMonth+1 : 1)-1;
+		System.out.print("Enter the season start day: ");
+		selectedDay = promptInt(selectedYear == currentYear && selectedMonth == currentMonth ? currentDay : 1);
+		
+		selectedDate.set(selectedYear, selectedMonth, selectedDay);
+		
+		return selectedDate;
+	}
+	
+	private static List<Referee> generateRefereeList(int numReferees) {
 		List<Referee> referees = new LinkedList<>();
 		Random r = new Random();
-		int numReferees = 10;
 		
 		for (int i=0; i < numReferees; i ++) {
 			referees.add(new Referee(r.nextInt(999999999) + 111111111, i==0));
@@ -88,58 +109,4 @@ public class Server {
 		
 		return referees;
 	}
-	
-	/*
-	static int userPick() throws InterruptedException
-    {
-        int choice = 0;
-        try
-        {
-            choice = console.nextInt();
-        }
-        catch(InputMismatchException e)
-        {
-            System.out.println("Please input a number");
-            TimeUnit.SECONDS.sleep(1);
-            console.next();
-        }
-        return choice;
-    }
-	
-	static String userString() throws InterruptedException {
-        String newString = null;
-        try {
-            newString = console.nextLine();
-        } catch (InputMismatchException e) {
-            System.out.println("Please type a number");
-            TimeUnit.SECONDS.sleep(1);
-            console.next();
-        }
-        return newString;
-    }
-
-    static void passwordSet() throws InterruptedException
-    {
-        System.out.println("Please set a super referee password:");
-        System.out.println();
-        globalPassword = userString();
-        System.out.println("Password is set to: " + globalPassword);
-        TimeUnit.SECONDS.sleep(1);
-        int userPick;
-        System.out.println("Are you sure you want to use this password?\n1. yes\n2. no");
-        userPick = userPick();
-        while (userPick != 1)
-        {
-            System.out.println("Please set a super referee password:");
-            System.out.println();
-            globalPassword = userString();
-            System.out.println("Password is set to: " + globalPassword);
-            TimeUnit.SECONDS.sleep(1);
-            System.out.println("Are you sure you want to use this password?\n1. yes\n2. no");
-            userPick = userPick();
-        }
-        TimeUnit.SECONDS.sleep(1);
-        System.out.println("Password is set");
-        TimeUnit.SECONDS.sleep(1);
-    }*/
 }
